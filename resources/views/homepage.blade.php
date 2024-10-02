feb68704842005abea4ae52d77a6e221eb9e0de2
tanzimk62 <tanzimk62@gmail.com>
2024-10-02 11:45:16 +0600

    commit korlam

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -41,7 +41,7 @@
               <label for="password-register-confirm" class="text-muted mb-1"><small>Confirm Password</small></label>
               <input name="password_confirmation" id="password-register-confirm" class="form-control" type="password" placeholder="Confirm password" value="{{old('password_confirmation')}}"/>
               @error('password_confirmation')
-              <p class="text-danger">{{$message}}</p>
+              <p class="text-danger">{{$message}} ok</p>
               @enderror
             </div>
 
